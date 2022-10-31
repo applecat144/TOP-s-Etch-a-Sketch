468e01270816b1d74faa4d4395d563d36e012acf
applecat144 <nathi.eve.online@gmail.com>
2022-10-31 20:43:41 +0100

    resize screen. Add draw only on click effect.

--- script.js
+++ script.js
@@ -12,15 +12,24 @@ for(i=0; i< (pixelPerSide**2); i++) {
 
 let allPixels = document.querySelectorAll('.pixel');
 let checkState = 0;
+let addColor = function(e) {this.style.backgroundColor = 'black';}
 
-window.addEventListener('mousedown', () => {allPixels.forEach((pixel) => checkHover(pixel))});
+window.addEventListener('mousedown', () => {checkState = 0; allPixels.forEach((pixel) => checkHover(pixel))}, true);
+window.addEventListener('mouseup', () => {checkState = 1; allPixels.forEach((pixel) => checkHover(pixel))}, true);
 
 
 
 function checkHover(pixel) {
 
-    const addColor = () => {pixel.style.backgroundColor = 'black';};
 
-    pixel.addEventListener('mouseover', addColor)
+    switch(checkState) {
+        case 0: pixel.addEventListener('mouseover', addColor);
+        console.log("test");
+        break;
+
+        case 1: pixel.removeEventListener('mouseover', addColor);
+        console.log("test2");
+        break;
+    }
 
 }
